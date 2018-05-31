42d690e7615583f463334ca93e905caf5860cbf4
Bryan Ngo <bngo@ucdavis.edu>
2018-05-31 12:08:33 -0700

    fixed linting errors

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-const config = require("./config.json");
-const argv = require('yargs').argv;
+const config = require('./config.json')
+const argv = require('yargs').argv
 
-let request = require('request');
-let apiKey = config.token;
-let city = argv.c || 'fairfield';
-let url = 'http://api.openweathermap.org/data/2.5/weather?q='+ city + '&units=imperial&appid=' + apiKey;
+let request = require('request')
+let apiKey = config.token
+let city = argv.c || 'fairfield'
+let url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city +
+'&units=imperial&appid=' + apiKey
 
-
-request(url, function(err, response, body) {
-	if(err) {
-		console.log('error:', error);
-	} else {
-	let weather = JSON.parse(body);
-	let message = 'Its ' + (weather.main.temp) + ' degrees in ' + (weather.name);
-	console.log(message);
-	}
-});
-
+request(url, function (err, response, body) {
+  if (err) {
+    console.log('error:', err)
+  } else {
+    let weather = JSON.parse(body)
+    let message = 'Its ' + (weather.main.temp) + ' degrees in ' + (weather.name)
+    console.log(message)
+  }
+})
